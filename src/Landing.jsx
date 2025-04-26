9231db4d77b34181bd080f9195d2df12475f2070
Daryl Doctora <dardoctora@gmail.com>
2025-04-27 01:23:23 +0800

    Landing screen

---
+++ src/Landing.jsx
@@ -0,0 +1,17 @@
+export default function Landing() {
+    return (
+        <div className="landing-body">
+            <div className="landing-side">
+                <img src="assets/landing_logo_icon_mini.png" />
+            </div>
+            <div className="landing-elements">
+                <img src="assets/landing_logo.png"/>
+
+                <div className="landing-buttons">
+                    <button className="login-btn">&nbsp;LOG IN</button>
+                    <button className="signup-btn">&nbsp;SIGN UP</button>
+                </div>
+            </div>
+        </div>
+    );
+}
